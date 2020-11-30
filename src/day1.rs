d5339052ee15c562693faaa2b80249d8703e5683
kalleakerblom <kalleakerblom@gmail.com>
2020-11-30 20:13:17 +0100

    Start repo for aoc2020

---
+++ src/day1.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    #[test]
+    fn example() {
+        let inp = "".to_string();
+    }
+    fn part1() {
+        let inp = fs::read_to_string("input/day01");
+    }
+}
